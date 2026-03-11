55b2481059908d7191aa287b0a006cd1ad0d94b5
Wrod <wrodtech@gmail.com>
2026-03-11 12:45:33 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'site-assistant-v2';
+const CACHE_NAME = 'site-Companion-v2';
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',        // your main HTML file (rename if needed)
